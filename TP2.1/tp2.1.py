c103bdcf6d74c16ed35ed239c00e04dd80bf325a
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2025-05-14 17:43:57 -0300

    PASAJE DE LOS MÉTODOS DEL ARCHIVO "funciones.py" a "tp2.1.py" PARA LA ENTREGA FINAL

--- TP2.1/tp2.1.py
+++ TP2.1/tp2.1.py
@@ -3,8 +3,36 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chisquare, norm, chi2
-from math import sqrt, isnan
-from funciones import obtener_numeros_centrales, generar_valor_a, son_coprimos, evaluar_test, generar_valor_binario
+from math import sqrt, isnan, gcd
+#from funciones import obtener_numeros_centrales, generar_valor_a, son_coprimos, evaluar_test, generar_valor_binario
+
+def obtener_numeros_centrales(valor):
+  return ((valor // 100) % 10000)
+
+def generar_valor_a(n):
+  return (4*n - 1)
+
+def son_coprimos(c, m):
+    return gcd(c, m) == 1
+
+def evaluar_test(resultado):
+   if(resultado < 0.01):
+      return "FALSE"
+   else:
+      return "TRUE"
+   
+def generar_valor_binario(n):
+   n = str(bin(n))
+   n = n.split('b')
+   n = n[1]
+
+   sum = 0
+   for i in range(len(n)):
+      sum += int(n[i])
+   
+   bin_final = 0 if (sum % 2) == 0 else 1
+   
+   return bin_final
 
 # Validamos que la ejecución del programa sea correcta
 if(len(sys.argv) != 3 or sys.argv[1] != '-s'):
